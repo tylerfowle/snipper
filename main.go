bfe574400f80395f91c828ea30ca187d83106814
tylerfowle <tylerfowle@gmail.com>
2017-06-27 17:49:35 -0500

    update file structure, add file looping

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
 type snippetJSON struct {
@@ -16,7 +17,10 @@ type snippetJSON struct {
 	File        string
 }
 
-var filePath = "./example.info"
+var outputPath = "./output/"
+var inputPath = "./input/"
+
+// var filePath = "./example.info"
 var err = ""
 
 // error checking helper function
@@ -33,35 +37,84 @@ func main() {
 	check(err)
 	defer WriteFile.Close()
 
-	fmt.Printf("// reading file %s\n", filePath)
+	for i, fileArray := range loopFiles() {
 
-	file, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		fmt.Printf("// error while reading file %s\n", filePath)
-		fmt.Printf("File error: %v\n", err)
-		os.Exit(1)
-	}
+		fmt.Printf("// reading file %s\n", fileArray)
 
-	fmt.Println("// defining array of struct snippetJSON")
-	var snippet []snippetJSON
+		file, err := ioutil.ReadFile(fileArray)
+		if err != nil {
+			fmt.Printf("// error while reading file %s\n", fileArray)
+			fmt.Printf("File error: %v\n", err)
+			os.Exit(1)
+		}
 
-	err = json.Unmarshal(file, &snippet)
-	if err != nil {
-		fmt.Println("error:", err)
-		os.Exit(1)
-	}
+		fmt.Println("// defining array of struct snippetJSON")
+		var snippet []snippetJSON
 
-	fmt.Println("// loop over array of structs of snippetJSON")
-	for i := range snippet {
-		// fmt.Printf("The ship '%s' first appeared on '%s'\n", snippet[i].Name, snippet[i].Trigger)
-		fmt.Printf("Title: '%s'\n", snippet[i].Title)
-		fmt.Printf("Name: '%s'\n", snippet[i].Name)
-		fmt.Printf("Trigger: '%s'\n", snippet[i].Trigger)
-		fmt.Printf("Description: '%s'\n", snippet[i].Description)
-		fmt.Printf("File: '%s'\n", snippet[i].File)
+		err = json.Unmarshal(file, &snippet)
+		if err != nil {
+			fmt.Println("error:", err)
+			os.Exit(1)
+		}
 
-		fmt.Fprintf(WriteFile, "snippet '%s' '%s'", snippet[i].Trigger, snippet[i].Description)
+		fmt.Println("// loop over array of structs of snippetJSON")
 
+		for i := range snippet {
+			// fmt.Printf("The ship '%s' first appeared on '%s'\n", snippet[i].Name, snippet[i].Trigger)
+			fmt.Printf("Title: '%s'\n", snippet[i].Title)
+			fmt.Printf("Name: '%s'\n", snippet[i].Name)
+			fmt.Printf("Trigger: '%s'\n", snippet[i].Trigger)
+			fmt.Printf("Description: '%s'\n", snippet[i].Description)
+			fmt.Printf("File: '%s'\n", snippet[i].File)
+
+			// fmt.Fprintf(WriteFile, "snippet '%s' '%s'", snippet[i].Trigger, snippet[i].Description)
+		}
+
+		fmt.Println(i)
 	}
 
 }
+
+func loopFiles() []string {
+
+	fileList := []string{}
+	err := filepath.Walk(inputPath, func(path string, f os.FileInfo, err error) error {
+		if f.IsDir() {
+			return nil
+		}
+		ignored, err := filepath.Match(".*", f.Name())
+		if err != nil {
+			return err
+		}
+		if !ignored {
+			fileList = append(fileList, path)
+		}
+		return nil
+	})
+	check(err)
+
+	// for i, file := range fileList {
+	// 	fmt.Println(file)
+	// }
+
+	return fileList
+}
+
+// func VisitFile(fp string, fi os.FileInfo, err error) error {
+// 	if err != nil {
+// 		fmt.Println(err) // can't walk here,
+// 		return nil       // but continue walking elsewhere
+// 	}
+// 	if fi.IsDir() {
+// 		return nil // not a file.  ignore.
+// 	}
+// 	// ignore hidden files
+// 	ignored, err := filepath.Match(".*", fi.Name())
+// 	if err != nil {
+// 		return err // this is fatal.
+// 	}
+// 	if !ignored {
+// 		fmt.Println(fp)
+// 	}
+// 	return nil
+// }
