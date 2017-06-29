a082d3c9abb492aada95dc355d475e83e6411db1
tylerfowle <tylerfowle@gmail.com>
2017-06-29 14:24:24 -0500

    rework

--- main.go
+++ main.go
@@ -33,99 +33,73 @@ func check(e error) {
 }
 
 // main
+
 func main() {
 
-	var WriteFile, err = os.Create("snip.txt")
-	check(err)
-	defer WriteFile.Close()
-
-	for i, fileArray := range loopFiles() {
-
-		fmt.Printf("// reading file %s\n", fileArray)
-
-		file, err := ioutil.ReadFile(fileArray)
-		if err != nil {
-			fmt.Printf("// error while reading file %s\n", fileArray)
-			fmt.Printf("File error: %v\n", err)
-			os.Exit(1)
-		}
-
-		fmt.Println("// defining array of struct snippetJSON")
-		var snippet []snippetJSON
-
-		err = json.Unmarshal(file, &snippet)
-		if err != nil {
-			fmt.Println("error:", err)
-			os.Exit(1)
-		}
-
-		fmt.Println("// loop over array of structs of snippetJSON")
-
-		for i := range snippet {
-			// fmt.Printf("The ship '%s' first appeared on '%s'\n", snippet[i].Name, snippet[i].Trigger)
-			fmt.Printf("Title: ")
-			color.Green("'%s'/n", snippet[i].Title)
-
-			fmt.Printf("Name: ")
-			color.Green("'%s'\n", snippet[i].Name)
-
-			fmt.Printf("Trigger: ")
-			color.Green("'%s'\n", snippet[i].Trigger)
-
-			fmt.Printf("Description: ")
-			color.Green("'%s'\n", snippet[i].Description)
-
-			fmt.Printf("File: ")
-			color.Green("'%s'\n", snippet[i].File)
-
-			// fmt.Fprintf(WriteFile, "snippet '%s' '%s'", snippet[i].Trigger, snippet[i].Description)
-		}
-
-		fmt.Println(i)
-	}
-
-}
-
-func loopFiles() []string {
-
 	fileList := []string{}
-	err := filepath.Walk(inputPath, func(path string, f os.FileInfo, err error) error {
-		if f.IsDir() {
+	err := filepath.Walk(inputPath, func(path string, file os.FileInfo, err error) error {
+		if file.IsDir() {
 			return nil
 		}
-		matched, err := filepath.Match("*.info", f.Name())
+		matched, err := filepath.Match("*.info", file.Name())
 		if err != nil {
 			return err
 		}
 		if matched {
 			fileList = append(fileList, path)
+
+			var WriteFile, err = os.Create("output/" + file.Name())
+			check(err)
+			defer WriteFile.Close()
+
+			filejson, err := ioutil.ReadFile(path)
+			if err != nil {
+				fmt.Printf("// error while reading file %s\n", path)
+				fmt.Printf("File error: %v\n", err)
+				os.Exit(1)
+			}
+
+			var snippet []snippetJSON
+
+			err = json.Unmarshal(filejson, &snippet)
+			if err != nil {
+				fmt.Println("error:", err)
+				os.Exit(1)
+			}
+
+			for i := range snippet {
+
+				fmt.Printf("Filename: ")
+				fmt.Printf(file.Name() + "\n")
+
+				fmt.Printf("Path: ")
+				fmt.Printf(path + "\n")
+
+				fmt.Printf("\n")
+
+				fmt.Printf("Title: ")
+				color.Green("'%s'\n", snippet[i].Title)
+
+				fmt.Printf("Name: ")
+				color.Green("'%s'\n", snippet[i].Name)
+
+				fmt.Printf("Trigger: ")
+				color.Green("'%s'\n", snippet[i].Trigger)
+
+				fmt.Printf("Description: ")
+				color.Green("'%s'\n", snippet[i].Description)
+
+				fmt.Printf("File: ")
+				color.Green("'%s'\n", snippet[i].File)
+
+				fmt.Printf("\n\n")
+
+				// fmt.Fprintf(WriteFile, "snippet '%s' '%s'", snippet[i].Trigger, snippet[i].Description)
+			}
+
 		}
 		return nil
 	})
 	check(err)
 
-	// for i, file := range fileList {
-	// 	fmt.Println(file)
-	// }
-
-	return fileList
 }
-
-// func VisitFile(fp string, fi os.FileInfo, err error) error {
-// 	if err != nil {
-// 		fmt.Println(err) // can't walk here,
-// 		return nil       // but continue walking elsewhere
-// 	}
-// 	if fi.IsDir() {
-// 		return nil // not a file.  ignore.
-// 	}
-// 	// ignore hidden files
-// 	ignored, err := filepath.Match(".*", fi.Name())
-// 	if err != nil {
-// 		return err // this is fatal.
-// 	}
-// 	if !ignored {
-// 		fmt.Println(fp)
-// 	}
-// 	return nil
-// }
